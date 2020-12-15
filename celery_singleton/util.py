9ee8341f6b8675e969813bd5215c1953c29161d1
sakorn <sakorn@acommerce.asia>
2020-12-15 17:30:03 +0700

    str() as default dumps function

--- celery_singleton/util.py
+++ celery_singleton/util.py
@@ -5,8 +5,8 @@ from hashlib import md5
 def generate_lock(
     task_name, task_args=None, task_kwargs=None, key_prefix="SINGLETONLOCK_"
 ):
-    str_args = json.dumps(task_args or [], sort_keys=True)
-    str_kwargs = json.dumps(task_kwargs or {}, sort_keys=True)
+    str_args = json.dumps(task_args or [], sort_keys=True, default=str)
+    str_kwargs = json.dumps(task_kwargs or {}, sort_keys=True, default=str)
     task_hash = md5((task_name + str_args + str_kwargs).encode()).hexdigest()
     key_prefix = key_prefix
     return key_prefix + task_hash
