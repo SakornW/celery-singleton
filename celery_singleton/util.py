9147251ff05293a4306a7f0e4064347b4bee0caf
sakorn <sakorn@acommerce.asia>
2020-12-16 10:05:00 +0700

    alpha version of pickle support

--- celery_singleton/util.py
+++ celery_singleton/util.py
@@ -5,6 +5,7 @@ from hashlib import md5
 def generate_lock(
     task_name, task_args=None, task_kwargs=None, key_prefix="SINGLETONLOCK_"
 ):
+    # default is a function applied to objects that aren't serializable.
     str_args = json.dumps(task_args or [], sort_keys=True, default=str)
     str_kwargs = json.dumps(task_kwargs or {}, sort_keys=True, default=str)
     task_hash = md5((task_name + str_args + str_kwargs).encode()).hexdigest()
---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='celery-singleton-internal',
+      version='0.3',
+      description='Extend opensource version with pickle support',
+      url='https://github.com/SakornW/celery-singleton',
+      packages=['celery_singleton'],)
