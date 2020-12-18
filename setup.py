23c4e853b022c532a70199808dd1b7604eab00af
sakorn <sakorn@acommerce.asia>
2020-12-18 10:46:10 +0700

    revise version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='celery-singleton-internal',
-      version='0.3',
+setup(name='celery-singleton',
+      version='0.4',
       description='Extend opensource version with pickle support',
       url='https://github.com/SakornW/celery-singleton',
       packages=['celery_singleton'],)
